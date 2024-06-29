ae883d2c533dc245cce861dfa39ac90dc1e035f0
leicaflorian <florian.leica@gmail.com>
2024-06-29 11:07:04 +0200

    Updates tests

--- tests/Feature/AuthorTest.php
+++ tests/Feature/AuthorTest.php
@@ -2,12 +2,16 @@
 	
 	namespace Tests\Feature;
 	
+	use App\Models\Author;
 	use Tests\TestCase;
 	
 	class AuthorTest extends TestCase {
-		
 		private array $basic_headers = [
 			'Accept' => 'application/json',
+		];
+		
+		private array $basic_auth_headers = [
+			'X-API-KEY' => 'c3e9f6e2-6b14-4a4e-b2d1-5ef5bb1cfc6a',
 		];
 		
 		private array $basic_error_structure = [
@@ -26,9 +30,22 @@
 			'updated_at',
 		];
 		
+		private function get_correct_headers(): array {
+			return array_merge($this->basic_headers, $this->basic_auth_headers);
+		}
+		
+		public function test_authorization(): void {
+			$response = $this->withHeaders($this->basic_headers)->get('/api/authors');
+			
+			$response->assertStatus(403)
+				->assertJsonStructure($this->basic_error_structure)
+				->assertJson([
+					"message" => "You are unauthorized. Please check your API key."
+				]);
+		}
 		
 		public function test_index(): void {
-			$response = $this->get('/api/authors');
+			$response = $this->withHeaders($this->get_correct_headers())->get('/api/authors');
 			
 			$response->assertStatus(200)
 				->assertJsonStructure([
@@ -46,22 +63,22 @@
 		}
 		
 		public function test_show(): void {
-			$response = $this->withHeaders($this->basic_headers)->get('/api/authors/1');
+			$response = $this->withHeaders($this->get_correct_headers())->get('/api/authors/1');
 			
 			$response->assertStatus(200)
 				->assertJsonStructure($this->basic_author_structure);
 		}
 		
 		public function test_show_fail(): void {
-			$response = $this->withHeaders($this->basic_headers)->get('/api/authors/99999999');
+			$response = $this->withHeaders($this->get_correct_headers())->get('/api/authors/99999999');
 			
 			$response->assertStatus(404)
 				->assertJsonStructure($this->basic_error_structure);
 		}
 		
 		public function test_store(): void {
-			$response = $this->withHeaders($this->basic_headers)->post('/api/authors', [
-				'firstname' => 'John',
+			$response = $this->withHeaders($this->get_correct_headers())->post('/api/authors', [
+				'firstname' => '[test] - John',
 				'lastname'  => 'Doe',
 			]);
 			
@@ -70,7 +87,7 @@
 		}
 		
 		public function test_store_fail(): void {
-			$response = $this->withHeaders($this->basic_headers)->post('/api/authors', [
+			$response = $this->withHeaders($this->get_correct_headers())->post('/api/authors', [
 				'firstname' => 'John',
 			]);
 			
@@ -83,5 +100,46 @@
 						]
 					]
 				]);
+		}
+		
+		public function test_update(): void {
+			$last_author = Author::where("firstname", "[test] - John")->latest()->first();
+			
+			$response = $this->withHeaders($this->get_correct_headers())->put("/api/authors/$last_author->id", [
+				'lastname' => 'Verdi',
+			]);
+			
+			$response->assertStatus(200)
+				->assertJsonStructure($this->basic_author_structure)
+				->assertJson([
+					'firstname' => '[test] - John',
+					'lastname'  => 'Verdi',
+				]);
+		}
+		
+		public function test_update_fail(): void {
+			$last_author = Author::where("firstname", "[test] - John")->latest()->first();
+			
+			$response = $this->withHeaders($this->get_correct_headers())->put("/api/authors/$last_author->id", [
+				'firstname' => 22,
+			]);
+			
+			$response->assertStatus(422)
+				->assertJson([
+					"message" => "The firstname field must be a string.",
+					"errors"  => [
+						"firstname" => [
+							"The firstname field must be a string."
+						]
+					]
+				]);
+		}
+		
+		public function test_destroy(): void {
+			$last_author = Author::where("firstname", "[test] - John")->latest()->first();
+			
+			$response = $this->withHeaders($this->get_correct_headers())->delete("/api/authors/$last_author->id");
+			
+			$response->assertNoContent();
 		}
 	}
--- tests/Feature/BookTest.php
+++ tests/Feature/BookTest.php
@@ -2,6 +2,7 @@
 	
 	namespace Tests\Feature;
 	
+	use App\Models\Book;
 	use Illuminate\Foundation\Testing\RefreshDatabase;
 	use Illuminate\Foundation\Testing\WithFaker;
 	use Tests\TestCase;
@@ -9,6 +10,10 @@
 	class BookTest extends TestCase {
 		private array $basic_headers = [
 			'Accept' => 'application/json',
+		];
+		
+		private array $basic_auth_headers = [
+			'X-API-KEY' => 'c3e9f6e2-6b14-4a4e-b2d1-5ef5bb1cfc6a',
 		];
 		
 		private array $basic_error_structure = [
@@ -29,9 +34,22 @@
 			'updated_at',
 		];
 		
+		private function get_correct_headers(): array {
+			return array_merge($this->basic_headers, $this->basic_auth_headers);
+		}
+		
+		public function test_authorization(): void {
+			$response = $this->withHeaders($this->basic_headers)->get('/api/authors');
+			
+			$response->assertStatus(403)
+				->assertJsonStructure($this->basic_error_structure)
+				->assertJson([
+					"message" => "You are unauthorized. Please check your API key."
+				]);
+		}
 		
 		public function test_index(): void {
-			$response = $this->get('/api/books');
+			$response = $this->withHeaders($this->get_correct_headers())->get('/api/books');
 			
 			$response->assertStatus(200)
 				->assertJsonStructure([
@@ -49,22 +67,22 @@
 		}
 		
 		public function test_show(): void {
-			$response = $this->withHeaders($this->basic_headers)->get('/api/books/1');
+			$response = $this->withHeaders($this->get_correct_headers())->get('/api/books/1');
 			
 			$response->assertStatus(200)
 				->assertJsonStructure($this->basic_book_structure);
 		}
 		
 		public function test_show_fail(): void {
-			$response = $this->withHeaders($this->basic_headers)->get('/api/books/99999999');
+			$response = $this->withHeaders($this->get_correct_headers())->get('/api/books/99999999');
 			
 			$response->assertStatus(404)
 				->assertJsonStructure($this->basic_error_structure);
 		}
 		
 		public function test_store(): void {
-			$response = $this->withHeaders($this->basic_headers)->post('/api/books', [
-				'title'       => 'My book',
+			$response = $this->withHeaders($this->get_correct_headers())->post('/api/books', [
+				'title' => '[test] - My book',
 				'subtitle'    => 'Some random subtitle',
 				'publisher'   => 'Some publisher',
 				'description' => 'Some description',
@@ -72,11 +90,17 @@
 			]);
 			
 			$response->assertCreated()
-				->assertJsonStructure($this->basic_book_structure);
+				->assertJsonStructure($this->basic_book_structure)
+				->assertJson([
+					'title'       => '[test] - My book',
+					'subtitle'    => 'Some random subtitle',
+					'publisher'   => 'Some publisher',
+					'description' => 'Some description',
+				]);
 		}
 		
 		public function test_store_fail(): void {
-			$response = $this->withHeaders($this->basic_headers)->post('/api/books', [
+			$response = $this->withHeaders($this->get_correct_headers())->post('/api/books', [
 				'subtitle'    => 'Some random subtitle',
 				'publisher'   => 'Some publisher',
 				'description' => 'Some description',
@@ -94,5 +118,49 @@
 						]
 					]
 				]);
+		}
+		
+		public function test_update(): void {
+			$last_book = Book::where("title", "[test] - My book")->latest()->first();
+			
+			$response = $this->withHeaders($this->get_correct_headers())->put("/api/books/$last_book->id", [
+				'subtitle'  => 'This subtitle has been changed',
+				'publisher' => 'Also a new publisher',
+			]);
+			
+			$response->assertStatus(200)
+				->assertJsonStructure($this->basic_book_structure)
+				->assertJson([
+					'title'       => '[test] - My book',
+					'subtitle'    => 'This subtitle has been changed',
+					'publisher'   => 'Also a new publisher',
+					'description' => 'Some description',
+				]);
+		}
+		
+		public function test_update_fail(): void {
+			$last_book = Book::where("title", "[test] - My book")->latest()->first();
+			
+			$response = $this->withHeaders($this->get_correct_headers())->put("/api/books/$last_book->id", [
+				'title' => 22,
+			]);
+			
+			$response->assertStatus(422)
+				->assertJson([
+					"message" => "The title field must be a string.",
+					"errors"  => [
+						"title" => [
+							"The title field must be a string."
+						]
+					]
+				]);
+		}
+		
+		public function test_destroy(): void {
+			$last_book = Book::where("title", "[test] - My book")->latest()->first();
+			
+			$response = $this->withHeaders($this->get_correct_headers())->delete("/api/books/$last_book->id");
+			
+			$response->assertNoContent();
 		}
 	}
